761ea4293105ec34c0b97c72f331cea050a641e6
eslam_magdy <eslam.magdy@argaam.com>
2020-02-07 20:45:00 +0200

    init ads

---
+++ nuxt.config.js
@@ -0,0 +1,74 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'https://securepubads.g.doubleclick.net/tag/js/gpt.js' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  gptAds: {
+    // Module options
+  },
+  modules: [
+    // Doc: https://buefy.github.io/#/documentation
+    'nuxt-buefy',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@ax2/gpt-ads-module',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configur ation
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+    <GptAd
+      :size="[300, 250]"
+      ad-unit="/96551095/V2_Masrawy_Desktop/Masrawy_Desktop_Homepage/Masrawy_Desktop_Homepage_SC1"
+    />
+  </section>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "D",
+
+  components: {}
+  // eslint-disable-next-line semi
+};
+</script>
